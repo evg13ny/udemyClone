63b0f585200d1390182dd78b3a9b50b461be95b8
Evgeny Polyakov <evg_pol@yahoo.com>
2022-07-24 16:40:46 +0300

    added database

--- app/core/config.php
+++ app/core/config.php
@@ -1,3 +1,20 @@
 <?php
 
-//
+// app info
+define('APP_NAME', 'Udemy App');
+define('APP_DESC', 'Free and paid tutorials');
+
+// database config
+
+if ($_SERVER['SERVER_NAME'] == 'localhost') {
+    // database config for local machine
+
+    define('DBHOST', 'localhost');
+    define('DBNAME', 'udemy_db');
+    define('DBUSER', 'root');
+    define('DBPASS', '');
+    define('DBDRIVER', 'mysql');
+} else {
+    // database config for live server
+
+}
