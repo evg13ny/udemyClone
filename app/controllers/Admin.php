fe49252c942ae8edaf393142302405c68d2d3c79
Evgeny Polyakov <evg_pol@yahoo.com>
2022-07-28 18:36:29 +0300

    added admin dashboard

---
+++ app/controllers/Admin.php
@@ -0,0 +1,12 @@
+<?php
+
+// Admin class
+
+class Admin extends Controller
+{
+    public function index()
+    {
+        $data['title'] = "Admin";
+        $this->view('admin/dashboard', $data);
+    }
+}
