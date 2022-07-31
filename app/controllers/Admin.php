e3b75f2a098c4aaa6fe774f7b80e21b748b543e9
Evgeny Polyakov <evg_pol@yahoo.com>
2022-08-01 01:09:05 +0300

    added resize images function

--- app/controllers/Admin.php
+++ app/controllers/Admin.php
@@ -42,6 +42,7 @@ class Admin extends Controller
                         if (in_array($_FILES['image']['type'], $allowed)) {
                             $destination = $folder . time() . $_FILES['image']['name'];
                             move_uploaded_file($_FILES['image']['tmp_name'], $destination);
+                            resize_image($destination);
                             $_POST['image'] = $destination;
 
                             if (file_exists($row->image)) {
--- app/core/functions.php
+++ app/core/functions.php
@@ -57,3 +57,61 @@ function str_to_url($url)
 
     return $url;
 }
+
+function resize_image($filename, $max_size = 700)
+{
+    $ext = explode(".", $filename);
+    $ext = strtolower(end($ext));
+
+    if (file_exists($filename)) {
+        switch ($ext) {
+            case 'png':
+                $image = imagecreatefrompng($filename);
+                break;
+
+            case 'jpg':
+            case 'jpeg':
+                $image = imagecreatefromjpeg($filename);
+                break;
+
+            default:
+                $image = imagecreatefromjpeg($filename);
+                break;
+        }
+
+        $src_w = imagesx($image);
+        $src_h = imagesy($image);
+
+        if ($src_w > $src_h) {
+            $dst_w = $max_size;
+            $dst_h = ($src_h / $src_w) * $max_size;
+        } else {
+            $dst_w = ($src_w / $src_h) * $max_size;
+            $dst_h = $max_size;
+        }
+
+        $dst_image = imagecreatetruecolor($dst_w, $dst_h);
+        imagecopyresampled($dst_image, $image, 0, 0, 0, 0, $dst_w, $dst_h, $src_w, $src_h);
+
+        imagedestroy($image);
+
+        switch ($ext) {
+            case 'png':
+                imagepng($dst_image, $filename);
+                break;
+
+            case 'jpg':
+            case 'jpeg':
+                imagejpeg($dst_image, $filename, 90);
+                break;
+
+            default:
+                imagejpeg($dst_image, $filename, 90);
+                break;
+        }
+
+        imagedestroy($dst_image);
+    }
+
+    return $filename;
+}
